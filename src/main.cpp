ac4ddabfaa883419a9e818aed926270eab697b4e
Volodymyr Gladun <vgladun@gmail.com>
2020-04-16 17:25:45 +0100

    added compile definitions to cmake files to use asio as header only

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,18 @@ using namespace boost::asio;
 
 int main() {
   try {
-    auto cli = Client{};
-    auto srv = Server{};
-
-    cli.create_endpoint<ip::tcp, ip::address_v4>(3333, "127.0.0.1");
+//    auto cli = Client{};
+//    cli.create_endpoint<ip::tcp, ip::address_v4>(3333, "127.0.0.1");
+    auto ec = boost::system::error_code{};
+    auto ip = ip::address_v4::from_string("127.0.0", ec);
+    if (ec) {
+      std::ostringstream msg;
+      msg << ip << "\n" << "value [" << ec.value() << "]\n"
+                << "message [" << ec.message() << "]";
+      throw (std::invalid_argument(msg.str()));
+    } else {
+      std::cout << ip;
+    }
   }
   catch (std::exception const &) {
      print_nested_exception();

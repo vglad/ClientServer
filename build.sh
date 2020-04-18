6058999ffeef938e10dfcf9112903291e13ef00c
Volodymyr Gladun <vgladun@gmail.com>
2020-04-18 21:49:18 +0100

    changed project name in build script

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ case ${BUILD_TYPE} in
 esac
 
 # Set project directories
-PROJECT_DIR=/opt/dev/ClientServer
+PROJECT_DIR=/opt/dev/Client-Server
 BUILD_DIR=${PROJECT_DIR}/build/cmake-build-${BUILD_TYPE}
 [[ ! -d "${BUILD_DIR}" ]] && ( mkdir -p ${BUILD_DIR} || return 1 )
 

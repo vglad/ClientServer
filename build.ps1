0763b3471576eedfdd503f402d7829b9f58a8589
Volodymyr Gladun <vgladun@gmail.com>
2020-04-22 21:26:34 +0100

    added test reporter to build.ps1

--- build.ps1
+++ build.ps1
@@ -111,5 +111,7 @@ if ($SkipTests -ne $true) {
   $wc = New-Object 'System.Net.WebClient'
   $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $BuildDir\testresults.xml))
 
+  & dir
+
   & $tests_path
 }
